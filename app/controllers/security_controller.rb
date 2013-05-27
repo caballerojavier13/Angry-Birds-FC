dda554b98a3b447fb50a992452379433701a4605
caballerojavier13 <caballerojavier13@gmail.com>
2013-05-26 22:46:50 -0300

    correccion direccion

--- app/controllers/security_controller.rb
+++ app/controllers/security_controller.rb
@@ -2,7 +2,7 @@ class SecurityController < ApplicationController
   before_filter :authorize
   def authorize
     unless Persona.find_by_id(session[:usuario_id])
-      redirect_to "/sessions/new", :notice => "Please log in"
+      redirect_to "/login", :notice => "Please log in"
     end
 
   end
