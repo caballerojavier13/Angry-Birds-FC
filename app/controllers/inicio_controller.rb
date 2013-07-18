82185dca0e54d11191becd21d06cb6335b51b6fb
caballerojavier13 <caballerojavier13@gmail.com>
2013-07-17 23:17:44 -0300

    Slideshow imagenes, modulo inicio. Mejora visual modulo videos.

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -1,8 +1,8 @@
 class InicioController < SecurityController
   def start
-	@persona = Persona.find(session[:usuario_id])
-  @videos =  Video.all.last(6)
-  @imagenes =  Imagen.all.last(5)
-  @noticias = Noticium.all.last(5)
+  	@persona = Persona.find(session[:usuario_id])
+    @videos =  Video.all.last(6)
+    @imagenes =  Imagen.all.last(5)
+    @noticias = Noticium.all.last(5)
   end
 end
