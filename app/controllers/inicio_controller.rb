15955731a4485c68c4a15e90d3705845153cd8a6
Javier Caballero <caballerojavier13@gmail.como>
2014-01-16 14:55:26 -0300

    mejora visualización noticia inicio

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -4,7 +4,7 @@ class InicioController < SecurityController
   	@persona = Persona.find(session[:usuario_id])
     @videos = Video.all.last(6)
     @imagenes = Imagen.all.last(5)
-    @noticias = Noticium.all.last(4)
+    @noticias = Noticium.all.order('id DESC').last(4)
     @notificaciones = Notification.where("read = ? AND persona_id = ?", false, session[:usuario_id])
   end
 end
