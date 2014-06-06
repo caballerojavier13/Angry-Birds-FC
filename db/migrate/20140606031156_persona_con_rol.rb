97ceea85c016f6b08d62f14056e9c113de9df0e7
Javier Caballero <caballerojavier13@gmail.como>
2014-06-06 19:20:39 -0300

    Problemas de migracion

--- db/migrate/20140606031156_persona_con_rol.rb
+++ db/migrate/20140606031156_persona_con_rol.rb
@@ -1,6 +1,6 @@
 class PersonaConRol < ActiveRecord::Migration
   def up
-    #add_column :personas, :role_id, :integer
+    add_column :personas, :role_id, :integer
     Persona.update_all(role: Role.where("name = 'Usuario Comun'"))
   end
 
